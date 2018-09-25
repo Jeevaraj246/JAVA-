029e37c942598cf7f95ac5876ee7d32031219d12
Jeeva.S <42260691+Jeevaraj246@users.noreply.github.com>
2018-09-25 09:46:31 +0530

    Create Write a program to print hello word n times given n.java

---
+++ JAVA Basics/Write a program to print hello word n times given n.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+class guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner in = new Scanner(System.in);
+		n = in.nextInt();
+		
+		for (int i = 0; i < n; i++) 
+			{
+				System.out.println("Hello");
+				}
+	}
+}
