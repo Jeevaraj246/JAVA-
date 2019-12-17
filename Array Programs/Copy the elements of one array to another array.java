948cc02a4ff57df77aa40f79213b9a0151651c88
Jeeva.S <42260691+Jeevaraj246@users.noreply.github.com>
2019-12-17 13:18:34 +0530

    Create Copy the elements of one array to another array.java

---
+++ Array Programs/Copy the elements of one array to another array.java
@@ -0,0 +1,12 @@
+class element{
+public static void main(String[]args){
+int[] a={1,2,3,4,5,6};
+int[] b=new int[a.length];
+for(int i=0;i<a.length;i++){
+b[i]=a[i];
+}
+for(int k=0;k<b.length;k++){
+System.out.print(b[k]+" ");
+}
+}
+}
